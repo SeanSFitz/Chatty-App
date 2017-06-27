547bdc1355dc64b8d3423b019b4487bf8d567062
SeanSFitz <sean.s.fitz@gmail.com>
2017-06-27 20:10:29 +0000

    working websocket connection and updating client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,18 +4,7 @@ import ChatBar from './components/ChatBar.jsx';
 
 let data = {
   currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
-  messages: [
-    {
-      id: 1,
-      username: "Bob",
-      content: "Has anyone seen my marbles?",
-    },
-    {
-      id: 2,
-      username: "Anonymous",
-      content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
-    }
-  ]
+  messages: []
 };
 
 class App extends Component {
@@ -27,15 +16,13 @@ class App extends Component {
 
   componentDidMount() {
     console.log("componentDidMount <App />");
-    setTimeout(() => {
-      console.log("Simulating incoming message");
-      // Add a new message to the list of messages in the data store
-      const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-      const messages = this.state.messages.concat(newMessage)
-      // Update the state of the app component.
-      // Calling setState will trigger a call to render() in App and all child components.
-      this.setState({messages: messages})
-    }, 3000);
+    this.socket =  new WebSocket("ws://localhost:3001", "protocol");
+      this.socket.onopen = () => {
+        console.log('connected');
+        this.socket.onmessage = () => {
+          this.addNewMessage(JSON.parse(event.data));
+        }
+      }
   }
 
   keyDownHandler(event) {
@@ -43,13 +30,18 @@ class App extends Component {
     if (event.keyCode === 13) {
       event.preventDefault();
       let message = {
-        id: 4,
         username: this.state.currentUser.name,
         content: event.target.value
       }
-      this.addNewMessage(message);
+      event.target.value = "";
+      this.sendMessage(message);
     }
   }
+
+  sendMessage(message) {
+    this.socket.send(JSON.stringify(message));
+  }
+
 
   addNewMessage(message) {
     let newMessageArray = this.state.messages;
